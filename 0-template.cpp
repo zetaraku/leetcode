fa3d9e6743c8e98fa9f1a6a6c0926b5ad28a954f
Raku Zeta <zetaraku@gmail.com>
2020-04-05 21:35:23 +0800

    Create 0-template.cpp

---
+++ 0-template.cpp
@@ -0,0 +1,12 @@
+// Title: <Insert Problem Name>
+// Description:
+//     <Copy description here.>
+//     <Indent every line.>
+// Link: <Put the link to the problem here>
+
+// Time complexity: <O(?)>
+// Space complexity: <O(?)>
+class Solution {
+public:
+    /* PASTE YOUR SOLUTION CODE */
+};
