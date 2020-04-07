a795f30ac5041fd9eeb5052c7dea911e8a2d45ad
Raku Zeta <zetaraku@gmail.com>
2020-04-07 20:37:46 +0800

    Create U-counting-elements.cpp

---
+++ U-counting-elements.cpp
@@ -0,0 +1,23 @@
+// Title: Counting Elements
+// Description:
+//     Given an integer array arr, count element x such that x + 1 is also in arr.
+//     If there're duplicates in arr, count them seperately.
+// Link: <Put the link to the problem here>
+
+// Time complexity: O(n)
+// Space complexity: O(n)
+class Solution {
+public:
+    int countElements(vector<int>& arr) {
+        // create a set with all elements in arr
+        std::unordered_set<int> appearedElements(arr.begin(), arr.end());
+        
+        // define count condition
+        auto isCounted = [&](int element) {
+            return appearedElements.count(element + 1) != 0;
+        };
+        
+        // count elements which match the condition
+        return std::count_if(arr.begin(), arr.end(), isCounted);
+    }
+};
