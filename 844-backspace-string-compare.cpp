55b0a6bf9af04eda1c6ef394aa2d1bf610ab9446
Raku Zeta <zetaraku@gmail.com>
2020-04-10 23:11:05 +0800

    Update 844-backspace-string-compare.cpp

--- 844-backspace-string-compare.cpp
+++ 844-backspace-string-compare.cpp
@@ -1,6 +1,8 @@
 // Title: Backspace String Compare
 // Description:
 //     Given two strings S and T, return if they are equal when both are typed into empty text editors. # means a backspace character.
+//     Follow up: Can you solve it in O(N) time and O(1) space?
+
 // Link: https://leetcode.com/problems/backspace-string-compare/
 
 // Time complexity: O(n)
@@ -8,28 +10,36 @@
 class Solution {
 public:
     bool backspaceCompare(string S, string T) {
-        // TODO: add aoms explaination on your solution
-        
-        for (auto itS = S.rbegin(), itT = T.rbegin(); true; itS++, itT++) {
-            executeDeletion(itS, S.rend()), executeDeletion(itT, T.rend());
-            if (itS == S.rend() && itT == T.rend())
+        // we compare backward so that each backspace can immediately cancel out one next character
+        for (auto ritS = S.rbegin(), ritT = T.rbegin(); true; ritS++, ritT++) {
+            // first we cancel out all characters with backspaces at the ends of both strings
+            skipCharsFromBack(ritS, S.rend()), skipCharsFromBack(ritT, T.rend());
+            
+            // two strings are equal if both strings are full matched
+            if (ritS == S.rend() && ritT == T.rend())
                 return true;
-            if (itS == S.rend() || itT == T.rend())
+            
+            // two strings are not equal if only one string is full matched but the other is not
+            if (ritS == S.rend() || ritT == T.rend())
                 return false;
-            if (*itS != *itT)
+            
+            // two strings are not equal if they mismatch on a character
+            if (*ritS != *ritT)
                 return false;
         }
     }
     
-    void executeDeletion(string::reverse_iterator &iter, string::reverse_iterator const &end) {
-        int del = 0;
-        for (auto &it = iter; it != end; it++) {
-            if (*it == '#')
-                del++;
-            else if (del != 0)
-                del--;
-            else
+    void skipCharsFromBack(string::reverse_iterator &rit, string::reverse_iterator const &rend) {
+        int numOfCharsToDelete = 0;
+        
+        while (rit != rend) {
+            if (*rit == '#') {
+                numOfCharsToDelete++, rit++;
+            } else if (numOfCharsToDelete > 0) {
+                numOfCharsToDelete--, rit++;
+            } else {
                 return;
+            }
         }
     }
 };
