818ac5c153d151a9bc1e4cc64e425655b9017b09
Raku Zeta <zetaraku@gmail.com>
2020-10-14 12:16:16 +0800

    Update 1143-longest-common-subsequence.cpp

--- 1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence.cpp
@@ -16,13 +16,22 @@ public:
         int n1 = text1.length();
         int n2 = text2.length();
         
-        vector<vector<int>> dp(n1+1, vector<int>(n2+1));{
+        // dp[i][j] = the length of longest common subsequence of text1[0,i) and text2[0,j)
+        vector<vector<int>> dp(n1+1, vector<int>(n2+1)); {
+            // dp[i][0] = 0
             for (int i = 0; i <= n1; i++)
                 dp.at(i).at(0) = 0;
+            // dp[0][j] = 0
             for (int j = 0; j <= n2; j++)
                 dp.at(0).at(j) = 0;
         }
         
+        /*
+            dp[i][j] = {
+                dp[i-1][j-1] + 1             if text1[i-1] == text2[j-1]
+                max(dp[i-1][j], dp[i][j-1])  if text1[i-1] != text2[j-1]
+            } for i > 0, j > 0
+        */
         for (int i = 0; i < n1; i++) {
             for (int j = 0; j < n2; j++) {
                 dp.at(i+1).at(j+1) = std::max(
