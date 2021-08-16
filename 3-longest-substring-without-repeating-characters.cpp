cd6a3ba3b4d314916170c1246a45b7e23148367d
Raku Zeta <zetaraku@gmail.com>
2021-08-16 21:43:45 +0800

    Create 3-longest-substring-without-repeating-characters.cpp

---
+++ 3-longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,35 @@
+// Title: Longest Substring Without Repeating Characters
+// Description:
+//     Given a string s, find the length of the longest substring without repeating characters.
+// Link: https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+// Time complexity: O(n)
+// Space complexity: O(n)
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        std::set<char> currentChars;
+        std::size_t beginIndex = 0;
+        std::size_t maxLength = 0;
+        
+        for (char c: s) {
+            if (currentChars.count(c) == 0) {
+                // insert the char
+                currentChars.insert(c);
+                // update the max length
+                maxLength = std::max(maxLength, currentChars.size());
+            } else {
+                // keep removing chars from the start until the repeating char is removed
+                while (true) {
+                    char charToRemove = s.at(beginIndex++);
+                    currentChars.erase(charToRemove);
+                    if (charToRemove == c) break;
+                }
+                // insert the char (this step can be merged with the loop above to save time)
+                currentChars.insert(c);
+            }
+        }
+        
+        return maxLength;
+    }
+};
