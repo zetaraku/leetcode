2de50152e1b14236d3d92ce832299bfec5091dd2
Raku Zeta <zetaraku@gmail.com>
2021-10-06 20:39:07 +0800

    Update 53-maximum-subarray.cpp

--- 53-maximum-subarray.cpp
+++ 53-maximum-subarray.cpp
@@ -9,24 +9,21 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int accumulatedSum = 0;
-        int maxAccumulatedSum = std::numeric_limits<int>::min();
+        int partialSum = 0;
+        int largestSum = INT_MIN;
         
-        // accumulate the sum from left to right
+        // greedy strategy
         for (int num: nums) {
-            // extend one number toward the right
-            accumulatedSum += num;
+            // add the value to the subarray
+            partialSum += num;
             
-            // update the max sum (should contain at least one number)
-            // this may be negative and should be done first!
-            if (accumulatedSum > maxAccumulatedSum)
-                maxAccumulatedSum = accumulatedSum;
-                
-            // empty the sum if it is negative (useless)
-            if (accumulatedSum < 0)
-                accumulatedSum = 0;
+            // update the max value
+            largestSum = std::max(partialSum, largestSum);
+            
+            // discard the subarray if the sum is already negative
+            if (partialSum < 0) partialSum = 0;
         }
         
-        return maxAccumulatedSum;
+        return largestSum;
     }
 };
