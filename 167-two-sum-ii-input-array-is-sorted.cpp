ccd103701ef3093c9bf658d9e98f9c92071efc2c
Raku Zeta <zetaraku@gmail.com>
2021-10-11 15:24:27 +0800

    Create 167-two-sum-ii-input-array-is-sorted.cpp

---
+++ 167-two-sum-ii-input-array-is-sorted.cpp
@@ -0,0 +1,34 @@
+// Title: Two Sum II - Input array is sorted
+// Description:
+//     Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number.
+//     Let these two numbers be numbers[index1] and numbers[index2] where 1 <= first < second <= numbers.length.
+//     Return the indices of the two numbers, index1 and index2, as an integer array [index1, index2] of length 2.
+//     The tests are generated such that there is exactly one solution. You may not use the same element twice.
+// Link: https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+
+// Time complexity: O(n)
+// Space complexity: O(1)
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        // pick the numbers at the start and at the end
+        int i = 0, j = numbers.size() - 1;
+        
+        while (true) {
+            // try to sum them up
+            int sum = numbers[i] + numbers[j];
+            
+            if (sum < target) {
+                // drop the one that is too small
+                i++;
+            } else if (sum > target) {
+                // drop the one that is too big
+                j--;
+            } else {
+                // found the solution (guaranteed)
+                std::vector<int> result = { 1 + i, 1 + j };
+                return result;
+            }
+        }
+    }
+};
