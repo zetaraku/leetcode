25cbe2ccc6695cca30cdc9336e29b6a1548c0723
Raku Zeta <zetaraku@gmail.com>
2022-08-03 01:13:23 +0800

    ✨ categorize problems based on difficulty

--- gen-readme.pl
+++ gen-readme.pl
@@ -1,33 +1,47 @@
 #!/usr/bin/perl
 use File::Basename;
 
+my %dtag = (
+  easy => '<span style="color: #00af9b;">Easy</span>',
+  medium => '<span style="color: #ffb800;">Medium</span>',
+  hard => '<span style="color: #ff2d55;">Hard</span>',
+);
+my %dcount = (
+  easy => 0,
+  medium => 0,
+  hard => 0,
+);
+
 my @fileinfo;
 
-foreach my $filepath (<code/*>) {
-  my $filename = basename($filepath);
-  my ($no) = $filename =~ /^(\d+)/;
-  my ($title, $link, $tc, $sc);
+foreach my $difficulty ('easy', 'medium', 'hard') {
+  foreach my $filepath (<code/$difficulty/*>) {
+    my $filename = basename($filepath);
+    my ($no) = $filename =~ /^(\d+)/;
+    my ($title, $link, $tc, $sc);
 
-  open my $fd, '<', $filepath or die "Can't open $filepath: $!";
-  while (<$fd>) {
-    $title = $1 if m"^(?://|#|--) Title: (.*)$";
-    $title =~ s/<Insert Problem Name>//;
-    $link = $1 if m"^(?://|#|--) Link: (.*)$";
-    $link =~ s/<Put the link to the problem here>//;
-    $tc = $1 if m"^(?://|#|--) Time complexity: (.*)$";
-    $sc = $1 if m"^(?://|#|--) Space complexity: (.*)$";
+    open my $fd, '<', $filepath or die "Can't open $filepath: $!";
+    while (<$fd>) {
+      $title = $1 if m"^(?://|#|--) Title: (.*)$";
+      $link = $1 if m"^(?://|#|--) Link: (.*)$";
+      $tc = $1 if m"^(?://|#|--) Time complexity: (.*)$";
+      $sc = $1 if m"^(?://|#|--) Space complexity: (.*)$";
+    }
+    close $fd;
+
+    push @fileinfo, {
+      difficulty => $dtag{$difficulty},
+      no => $no // 9999,
+      title => $title || undef,
+      link => $link || undef,
+      filename => $filename,
+      filepath => $filepath,
+      tc => $tc // 'N/A',
+      sc => $sc // 'N/A',
+    };
+
+    $dcount{$difficulty}++;
   }
-  close $fd;
-
-  push @fileinfo, {
-    no => $no // 9999,
-    title => $title || undef,
-    link => $link || undef,
-    filename => $filename,
-    filepath => $filepath,
-    tc => $tc // 'N/A',
-    sc => $sc // 'N/A',
-  };
 }
 
 print <<EOF;
@@ -35,10 +49,14 @@ print <<EOF;
 
 My [LeetCode](https://leetcode.com/) submissions
 
+- $dtag{easy}: $dcount{easy}
+- $dtag{medium}: $dcount{medium}
+- $dtag{hard}: $dcount{hard}
+
 ## Index
 
-| No. | Title | Code | Time complexity | Space complexity |
-| ---:| ----- | ---- | --------------- | ---------------- |
+| No. | Difficulty | Title | Code | Time complexity | Space complexity |
+| ---:| ---------- | ----- | ---- | --------------- | ---------------- |
 EOF
 
 foreach (sort { $a->{no} <=> $b->{no} } @fileinfo) {
@@ -46,6 +64,6 @@ foreach (sort { $a->{no} <=> $b->{no} } @fileinfo) {
   my $code = "[$_->{filename}](./$_->{filepath})";
 
   print <<LINE;
-| $_->{no} | $title | $code | `$_->{tc}` | `$_->{sc}` |
+| $_->{no} | $_->{difficulty} | $title | $code | `$_->{tc}` | `$_->{sc}` |
 LINE
 }
