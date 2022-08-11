7d8f82cc3fead7c2d04615f560c7d22b3ee654af
Raku Zeta <zetaraku@gmail.com>
2022-08-11 14:39:08 +0800

    Create 102-binary-tree-level-order-traversal.cpp

---
+++ code/medium/102-binary-tree-level-order-traversal.cpp
@@ -0,0 +1,63 @@
+// Title: Binary Tree Level Order Traversal
+// Description:
+//     Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
+// Link: https://leetcode.com/problems/binary-tree-level-order-traversal/
+
+// Time complexity: O(n)
+// Space complexity: O(n)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    std::vector<std::vector<int>> levelOrder(TreeNode *root) {
+        // remaining nodes to process
+        std::queue<TreeNode *> nodeQueue; {
+            // push the initial node (may be NULL)
+            nodeQueue.push(root);
+            // push an end-of-level mark (NULL)
+            nodeQueue.push(NULL);
+        }
+        
+        std::vector<std::vector<int>> result;
+        std::vector<int> currentLevel;
+        
+        while (true) {
+            // take out the next node to process
+            TreeNode *node = nodeQueue.front(); nodeQueue.pop();
+            
+            // end if an end-of-level mark (or a NULL root) is reached before a node
+            if (node == NULL) break;
+            
+            // append the value to the current level
+            currentLevel.push_back(node->val);
+            
+            // push the left node and right node to the queue (if any)
+            if (node->left != NULL) nodeQueue.push(node->left);
+            if (node->right != NULL) nodeQueue.push(node->right);
+            
+            // check if this node is at the end of the current level
+            if (nodeQueue.front() == NULL) {
+                // remove the end-of-level mark
+                nodeQueue.pop();
+                
+                // move the current level into the result and reset the current level
+                result.push_back(std::move(currentLevel));
+                currentLevel = std::vector<int>();
+                
+                // append an end-of-level mark to mark the end of the next level
+                nodeQueue.push(NULL);
+            }
+        }
+        
+        return result;
+    }
+};
