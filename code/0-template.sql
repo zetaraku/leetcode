ad97b42be95505b1c8bf7c0478637635846d401b
Raku Zeta <zetaraku@gmail.com>
2022-11-16 22:03:59 +0800

    Create 0-template.sql

---
+++ code/0-template.sql
@@ -0,0 +1,7 @@
+-- Title: <Insert Problem Name>
+-- Description:
+--     <Copy description here.>
+--     <Indent every line.>
+-- Link: <Put the link to the problem here>
+
+-- Write your MySQL query statement below
