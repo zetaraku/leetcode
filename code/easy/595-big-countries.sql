c6e3192cf04408a7236ff9f4c1730614e0ab51ab
Raku Zeta <zetaraku@gmail.com>
2022-11-16 22:08:09 +0800

    Update 595-big-countries.sql

--- code/easy/595-big-countries.sql
+++ code/easy/595-big-countries.sql
@@ -2,6 +2,7 @@
 -- Description:
 --     A country is big if it has an area of bigger than 3 million square km or a population of more than 25 million.
 --     Write a SQL solution to output big countries' name, population and area.
+--     Return the result table in any order.
 -- Link: https://leetcode.com/problems/big-countries/
 
 -- Write your MySQL query statement below
