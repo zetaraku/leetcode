636a517b4cb6ace1b4099c183f06fa55b68ab59d
Raku Zeta <zetaraku@gmail.com>
2022-11-17 10:30:11 +0800

    Create 584-find-customer-referee.sql

---
+++ code/easy/584-find-customer-referee.sql
@@ -0,0 +1,9 @@
+-- Title: Find Customer Referee
+-- Description:
+--     Write an SQL query to report the names of the customer that are not referred by the customer with id = 2.
+--     Return the result table in any order.
+-- Link: https://leetcode.com/problems/find-customer-referee/
+
+-- Write your MySQL query statement below
+SELECT name FROM Customer
+WHERE referee_id <> 2 OR referee_id IS NULL
