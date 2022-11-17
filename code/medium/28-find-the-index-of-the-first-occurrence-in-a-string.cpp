63b73cc914fd97638d5feb58ad5063ad2351d5e2
Raku Zeta <zetaraku@gmail.com>
2022-11-17 11:59:32 +0800

    Change information of Problem 28

--- code/medium/28-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ code/medium/28-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,8 +1,7 @@
-// Title: Implement strStr()
+// Title: Find the Index of the First Occurrence in a String
 // Description:
-//     Implement strStr().
-//     Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
-// Link: https://leetcode.com/problems/implement-strstr/
+//     Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+// Link: https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
 
 // Time complexity: O(m+n)
 // Space complexity: O(m)
