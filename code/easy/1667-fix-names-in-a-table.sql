6dd29fac61167078cfa816d2777f2adb43a5304a
Raku Zeta <zetaraku@gmail.com>
2022-11-18 10:39:05 +0800

    Create 1667-fix-names-in-a-table.sql

---
+++ code/easy/1667-fix-names-in-a-table.sql
@@ -0,0 +1,13 @@
+-- Title: Fix Names in a Table
+-- Description:
+--     Write an SQL query to fix the names so that only the first character is uppercase and the rest are lowercase.
+--     Return the result table ordered by user_id.
+-- Link: https://leetcode.com/problems/fix-names-in-a-table/
+
+-- Write your MySQL query statement below
+SELECT
+    user_id,
+    -- You can use INITCAP() instead if available
+    CONCAT(UPPER(SUBSTRING(name, 1, 1)), LOWER(SUBSTRING(name, 2))) AS name
+FROM Users
+ORDER BY user_id
