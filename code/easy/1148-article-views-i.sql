54de7481eb31166f34ef8e3c5e23c3354eddcc82
Raku Zeta <zetaraku@gmail.com>
2022-11-20 13:04:41 +0800

    Create 1148-article-views-i.sql

---
+++ code/easy/1148-article-views-i.sql
@@ -0,0 +1,11 @@
+-- Title: Article Views I
+-- Description:
+--     Write an SQL query to find all the authors that viewed at least one of their own articles.
+--     Return the result table sorted by id in ascending order.
+-- Link: https://leetcode.com/problems/article-views-i/
+
+-- Write your PL/SQL query statement below
+SELECT DISTINCT viewer_id AS id
+FROM Views
+WHERE viewer_id = author_id
+ORDER BY id ASC
