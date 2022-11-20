3a16a1241b697598f5e2cb73ea1b76e5daf35df7
Raku Zeta <zetaraku@gmail.com>
2022-11-20 13:00:28 +0800

    Create 1581-customer-who-visited-but-did-not-make-any-transactions.sql

---
+++ code/easy/1581-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -0,0 +1,11 @@
+-- Title: Customer Who Visited but Did Not Make Any Transactions
+-- Description:
+--     Write an SQL query to find the IDs of the users who visited without making any transactions and the number of times they made these types of visits.
+--     Return the result table sorted in any order.
+-- Link: https://leetcode.com/problems/customer-who-visited-but-did-not-make-any-transactions/
+
+-- Write your PL/SQL query statement below
+SELECT customer_id, COUNT(*) AS count_no_trans
+FROM Visits LEFT JOIN Transactions USING (visit_id)
+WHERE transaction_id IS NULL
+GROUP BY customer_id
