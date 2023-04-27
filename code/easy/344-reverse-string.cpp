3cdfa55f4019f8d89b42f043a4d13d8aaf6b8983
Raku Zeta <zetaraku@gmail.com>
2023-04-27 11:23:53 +0800

    Update 344-reverse-string.cpp

--- code/easy/344-reverse-string.cpp
+++ code/easy/344-reverse-string.cpp
@@ -8,9 +8,12 @@
 // Space complexity: O(1)
 class Solution {
 public:
-    void reverseString(vector<char>& s) {
+    void reverseString(std::vector<char> &s) {
+        // handle edge case
+        if (s.size() == 0) return;
+
         // swap each pair from both sides
-        for (int i = 0, j = s.size() - 1; i < j; ++i, --j) {
+        for (std::size_t i = 0, j = s.size() - 1; i < j; ++i, --j) {
             std::swap(s[i], s[j]);
         }
     }
