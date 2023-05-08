042ba50dbc8a64cc5a7149ed4de8a5129c9af2c4
Raku Zeta <zetaraku@gmail.com>
2023-05-08 22:13:38 +0800

    Update 1046-last-stone-weight.cpp

--- code/easy/1046-last-stone-weight.cpp
+++ code/easy/1046-last-stone-weight.cpp
@@ -1,12 +1,12 @@
 // Title: Last Stone Weight
 // Description:
-//     We have a collection of stones, each stone has a positive integer weight.
-//     Each turn, we choose the two heaviest stones and smash them together.
-//     Suppose the stones have weights x and y with x <= y.
-//     The result of this smash is:
-//         If x == y, both stones are totally destroyed;
-//         If x != y, the stone of weight x is totally destroyed, and the stone of weight y has new weight y-x.
-//     At the end, there is at most 1 stone left.  Return the weight of this stone (or 0 if there are no stones left.)
+//     You are given an array of integers stones where stones[i] is the weight of the i-th stone.
+//     We are playing a game with the stones. On each turn, we choose the heaviest two stones and smash them together.
+//     Suppose the heaviest two stones have weights x and y with x <= y. The result of this smash is:
+//         If x == y, both stones are destroyed, and
+//         If x != y, the stone of weight x is destroyed, and the stone of weight y has new weight y - x.
+//     At the end of the game, there is at most one stone left.
+//     Return the weight of the last remaining stone. If there are no stones left, return 0.
 // Link: https://leetcode.com/problems/last-stone-weight/
 
 // Time complexity: O(n*log(n))
