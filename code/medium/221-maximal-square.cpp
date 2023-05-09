33b3cbb9c06ef5da0cee8e5dcbd5b14d5f47cff7
Raku Zeta <zetaraku@gmail.com>
2023-05-09 16:43:15 +0800

    Update 221-maximal-square.cpp

--- code/medium/221-maximal-square.cpp
+++ code/medium/221-maximal-square.cpp
@@ -1,32 +1,30 @@
 // Title: Maximal Square
 // Description:
-//     Given a 2D binary matrix filled with 0's and 1's, find the largest square containing only 1's and return its area.
+//     Given an m x n binary matrix filled with 0's and 1's, find the largest square containing only 1's and return its area.
 // Link: https://leetcode.com/problems/maximal-square/
 
-// Time complexity: O(r*c)
-// Space complexity: O(r*c)
+// Time complexity: O(m*n)
+// Space complexity: O(m*n)
 class Solution {
 public:
-    int maximalSquare(vector<vector<char>> &matrix) {
-        int r = matrix.size();
-        if (r == 0) return 0;
-        int c = matrix.at(0).size();
-        if (c == 0) return 0;
+    int maximalSquare(std::vector<std::vector<char>> &matrix) {
+        if (matrix.size() == 0 || matrix[0].size() == 0) return 0;
+
+        const std::size_t M = matrix.size();
+        const std::size_t N = matrix[0].size();
         
         // keep track of the side of the largest square
-        int maxSide = 0;
+        std::size_t maxSide = 0;
         
         // dp[i][j] = side of the largest square containing only 1's with (i, j) as the bottom-right corner
-        vector<vector<int>> dp(r, vector<int>(c)); {
-            // dp[i][0] = matrix[i][0] == '1' ? 1 : 0
-            for (int i = 0; i < r; i++) {
-                int side = dp.at(i).at(0) = matrix.at(i).at(0) == '1' ? 1 : 0;
-                maxSide = std::max(maxSide, side);
+        std::vector<std::vector<std::size_t>> dp(M, std::vector<std::size_t>(N)); {
+            for (std::size_t i = 0; i != M; ++i) {
+                dp[i][0] = (matrix[i][0] == '1' ? 1 : 0);
+                maxSide = std::max(dp[i][0], maxSide);
             }
-            // dp[0][j] = matrix[0][j] == '1' ? 1 : 0
-            for (int j = 0; j < c; j++) {
-                int side = dp.at(0).at(j) = matrix.at(0).at(j) == '1' ? 1 : 0;
-                maxSide = std::max(maxSide, side);
+            for (std::size_t j = 0; j != N; ++j) {
+                dp[0][j] = (matrix[0][j] == '1' ? 1 : 0);
+                maxSide = std::max(dp[0][j], maxSide);
             }
         }
         
@@ -36,20 +34,14 @@ public:
                 0                                              if matrix[i][j] != '1'
             } for i > 0, j > 0
         */
-        for (int i = 1; i < r; i++) {
-            for (int j = 1; j < c; j++) {
-                int side = dp.at(i).at(j) = (matrix.at(i).at(j) == '1' ?
-                    std::min({
-                        dp.at(i-1).at(j-1),
-                        dp.at(i).at(j-1),
-                        dp.at(i-1).at(j)
-                    }) + 1 : 0
-                );
-                maxSide = std::max(maxSide, side);
+        for (std::size_t i = 1; i < M; ++i) {
+            for (std::size_t j = 1; j < N; ++j) {
+                dp[i][j] = (matrix[i][j] == '1' ? std::min({ dp[i-1][j-1], dp[i][j-1], dp[i-1][j] }) + 1 : 0);
+                maxSide = std::max(dp[i][j], maxSide);
             }
         }
         
-        // return the max area
+        // return the max square area
         return maxSide * maxSide;
     }
 };
