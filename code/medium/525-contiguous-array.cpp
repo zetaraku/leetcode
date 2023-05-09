1777374a3281ab5365a0c01b57703bd664a75995
Raku Zeta <zetaraku@gmail.com>
2023-05-09 21:13:02 +0800

    Update 525-contiguous-array.cpp

--- code/medium/525-contiguous-array.cpp
+++ code/medium/525-contiguous-array.cpp
@@ -7,32 +7,34 @@
 // Space complexity: O(n)
 class Solution {
 public:
-    int findMaxLength(vector<int> &nums) {
-        unordered_map<int,int> firstOccurrenceOfSums; {
-            firstOccurrenceOfSums.emplace(0, -1);
+    int findMaxLength(std::vector<int> &nums) {
+        const std::size_t N = nums.size();
+
+        std::unordered_map<int, std::size_t> firstEndOfSums; {
+            // an empty sum of 0 ends at index 0
+            firstEndOfSums[0] = 0;
         }
         
-        int partialSumFromStart = 0;
-        int maxLengthOfPartialSum = 0;
+        int partialSum = 0;
+        std::size_t maxSubarrayLength = 0;
         
-        for (int i = 0; i != nums.size(); i++) {
+        for (std::size_t i = 0; i != N; ++i) {
             // we map 0 to -1 and 1 to +1 so that:
-            // if any partial sum equals 0, then the total of 0s is equal to the total of 1s in that range
+            // if any sum of subarray equals 0, then the total of 0s is equal to the total of 1s in that range
             // and if we reach a same sum seen before, then the partial sum between those two is equal to 0
-            partialSumFromStart += (nums.at(i) == 0 ? -1 : +1);
+            partialSum += (nums[i] == 0 ? -1 : +1);
             
-            if (firstOccurrenceOfSums.count(partialSumFromStart) != 0) {
-                // update the max length if we reach a same sum encountered before
-                maxLengthOfPartialSum = std::max(
-                    maxLengthOfPartialSum,
-                    i - firstOccurrenceOfSums.at(partialSumFromStart)
-                );
+            // check if we reach a same sum encountered before
+            if (firstEndOfSums.count(partialSum) != 0) {
+                // sum of subarray [firstEndOfSums[partialSum], (i+1)) = 0
+                std::size_t subarrayLength = (i+1) - firstEndOfSums[partialSum];
+                maxSubarrayLength = std::max(subarrayLength, maxSubarrayLength);
             } else {
-                // record the first occurrence of the sum if we haven't seen it before
-                firstOccurrenceOfSums.emplace(partialSumFromStart, i);
+                // record the end of first occurrence of the sum if we haven't seen it before
+                firstEndOfSums[partialSum] = (i+1);
             }
         }
         
-        return maxLengthOfPartialSum;
+        return (int) maxSubarrayLength;
     }
 };
