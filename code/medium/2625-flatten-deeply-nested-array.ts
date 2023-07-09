e5fae5af201deff7cc519f8b48b9570e5b9af082
Raku Zeta <zetaraku@gmail.com>
2023-07-09 21:06:33 +0800

    Create 2625-flatten-deeply-nested-array.ts

---
+++ code/medium/2625-flatten-deeply-nested-array.ts
@@ -0,0 +1,15 @@
+// Title: Flatten Deeply Nested Array
+// Description:
+//     Given a multi-dimensional array arr and a depth n, return a flattened version of that array.
+//     A multi-dimensional array is a recursive data structure that contains integers or other multi-dimensional arrays.
+//     A flattened array is a version of that array with some or all of the sub-arrays removed and replaced with the actual elements in that sub-array.
+//     This flattening operation should only be done if the current depth of nesting is less than n.
+//     The depth of the elements in the first array are considered to be 0.
+//     Please solve it without the built-in Array.flat method.
+// Link: https://leetcode.com/problems/flatten-deeply-nested-array/
+
+type MultiDimensionalArray = (number | MultiDimensionalArray)[];
+
+function flat(arr: MultiDimensionalArray, n: number): MultiDimensionalArray {
+  return n > 0 ? arr.flatMap((e) => Array.isArray(e) ? flat(e, n-1) : e) : arr;
+}
