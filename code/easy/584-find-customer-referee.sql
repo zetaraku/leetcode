fd00286f9380fe209085e5c4eb91c90cec5d454c
Raku Zeta <zetaraku@gmail.com>
2024-02-15 13:45:20 +0800

    Update 584-find-customer-referee.sql

--- code/easy/584-find-customer-referee.sql
+++ code/easy/584-find-customer-referee.sql
@@ -4,6 +4,6 @@
 --     Return the result table in any order.
 -- Link: https://leetcode.com/problems/find-customer-referee/
 
--- Write your MySQL query statement below
+-- Write your PostgreSQL query statement below
 SELECT name FROM Customer
 WHERE referee_id <> 2 OR referee_id IS NULL
