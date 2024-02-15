1e6f3ad6556daf0d12ea1c1f2c759b4f1de77385
Raku Zeta <zetaraku@gmail.com>
2024-02-15 13:43:48 +0800

    Update 1757-recyclable-and-low-fat-products.sql

--- code/easy/1757-recyclable-and-low-fat-products.sql
+++ code/easy/1757-recyclable-and-low-fat-products.sql
@@ -4,6 +4,6 @@
 --     Return the result table in any order.
 -- Link: https://leetcode.com/problems/recyclable-and-low-fat-products/
 
--- Write your MySQL query statement below
+-- Write your PostgreSQL query statement below
 SELECT product_id FROM Products
 WHERE low_fats = 'Y' AND recyclable = 'Y'
